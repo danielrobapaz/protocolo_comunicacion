3c35da832ee0597293f484c086f880998e06d7d9
Daniel Robayo <18-11086@usb.ve>
2023-10-08 22:44:58 -0400

    dios le da sus batallas mas dificiles a sus mejores guerreros

--- protocolo.c
+++ protocolo.c
@@ -8,29 +8,25 @@
 
 int n_nodes = 2;
 
-void write_pipe(int n_pipe, int *value_write) {
-    if (write(n_pipe, value_write, sizeof(int*))==-1) {
-        printf("error escribiendo al pipe\n");
-        exit(1);
-    }
-    close(n_pipe);
-}
-
-void read_pipe(int n_pipe, int *value_read) {
-    if (read(n_pipe, value_read, sizeof(int*))==-1) {
-        printf("error leyendo el pipe\n");
-        exit(1);
-    }
-}
 
 int main() {
-    int pipes[n_nodes][2], ch_pid, my_node, liters, msj;
+    int parent_to_child[n_nodes][2], child_to_parent[n_nodes][2];
+    int ch_pid, my_node, msj;
+    int liters_JG, liters_jp, JG, jp;
+
+    JG = 0;
+    jp = 1;
 
     my_node = -1;
     /* inicializamos los pipes*/
     for (int i=0; i<n_nodes; i++) {
-        if (pipe(pipes[i])) {
+        if (pipe(parent_to_child[i])) {
             printf("error en la creacion del pipe.\n");
+            exit(1);
+        }
+
+        if (pipe(child_to_parent[i])) {
+            printf("error en la creacion del pipe. \n");
             exit(1);
         }
     }
@@ -38,46 +34,95 @@ int main() {
     for (int i=0; i<n_nodes; i++) {
         /* creamos a los hijos */
         ch_pid = fork();
+        int liters=0;
 
         if (ch_pid) {
             /* caso del proceso padre*/
-            write_pipe(pipes[i][WRITE_END], &i);
+            write_from_parent_child(parent_to_child[i], &i);
         } else {
             /* caso del proceso hijo*/
-            read_pipe(pipes[i][READ_END], &my_node);
-            liters = 0;
+            read_from_parent_child(parent_to_child[i], &i);
+            
             break; /* se corta del ciclo para evitar generar hijos de mas*/
         }
     }
+    
+    if (my_node == -1) {
+        close(child_to_parent[0][WRITE_END]);
+        if (read(child_to_parent[0][READ_END], &msj, sizeof(int*))==-1){
+            printf("no se que poner\n");
+            exit(1);
+        }
+        close(child_to_parent[0][READ_END]);
 
-    if (my_node == -1) { /* padre*/
-        read_pipe(pipes[0][READ_END], &msj);
-        write_pipe(pipes[1][WRITE_END], &msj);
+        printf("msj = %d", msj);
+
+        close(parent_to_child[1][READ_END]);
+        if (write(parent_to_child[1][WRITE_END], &msj, sizeof(int*))==-1){
+            printf("hasta cuando papa dios\n");
+            exit(1);
+        }
+        close(parent_to_child[1][WRITE_END]);
+        //read_pipe(pipes[0][READ_END], &msj);
+        //write_pipe(pipes[1][WRITE_END], &msj);
     }
 
 
-    if (my_node==0) { /* JG */
-        /* llenar(JG, 5)*/
-        liters = 5;
+    if (my_node==0) { //JG
+        // llenar(JG, 5)
+        liters_JG = 5;
 
-        /* anadir (JG, jp, 3)*/
         msj = 3;
-        liters -= msj;
-        write_pipe(pipes[my_node][WRITE_END], &msj);
+        liters_JG -= msj;
         
-        printf("JG %d\n", liters);
+        close(parent_to_child[my_node][READ_END]);
+        close(child_to_parent[my_node][READ_END]);
+        if (write(child_to_parent[my_node][WRITE_END], &msj, sizeof(int*))==-1){
+            printf("uwu\n");
+            exit(1);
+        }
+        close(child_to_parent[my_node][WRITE_END]);
+
+        //write_pipe(pipes[my_node][WRITE_END], &msj);
+        
+        printf("JG %d\n", liters_JG);
     }
 
-
-    if (my_node==1){ /* jp */
-        read_pipe(pipes[my_node][READ_END], &msj);
-        liters += msj;
-        printf("jp %d\n", liters);
+    
+    if (my_node==1){
+        liters_jp = 0;
+        close(parent_to_child[my_node][WRITE_END]);
+        if(read(parent_to_child[my_node][READ_END], &msj, sizeof(int*))==-1){
+            printf("ausilio\n");
+            exit(1);
+        }
+        close(parent_to_child[my_node][READ_END]);
+        //read_pipe(pipes[my_node][READ_END], &msj);
+        liters_jp += msj;
     }
-
+    
     while (wait(NULL)>0) {
         ;
     }
 
     return 0;
+}
+
+void write_from_parent_child(int parent_pipe[], int *value) {
+    close(parent_pipe[READ_END]);
+    if (write(parent_pipe[WRITE_END], value, sizeof(int*))==-1) {
+        printf("error escribiendo el pipe\n");
+        exit(1);
+    }
+}
+
+
+void read_from_parent_child(int parent_pipe[], int *value){
+    close(parent_pipe[WRITE_END]);
+    if (read(parent_pipe[READ_END], value, sizeof(int*))==-1){
+        printf("error leyendo el pipe\n");
+        exit(1);
+    }
+
+    close(parent_pipe[READ_END]);
 }
